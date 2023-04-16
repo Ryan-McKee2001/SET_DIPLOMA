34309e26cb65d54be77200098d8339c898dac1df
Ryan <74019611+Ryan-McKee2001@users.noreply.github.com>
2023-04-16 19:20:23 +0100

    Updating report

---
+++ SeleniumTesting/src/test/java/tests/Popups.java
@@ -0,0 +1,13 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class Popups {
+    public static void main(String[] args) {
+
+        WebDriverManager edgeDriver = EdgeDriverManager.
+        WebDriver browser = new WebDriverM
+    }
+}
