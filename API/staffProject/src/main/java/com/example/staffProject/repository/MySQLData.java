3ced29ac1ac61c9c089d5aabd34ec415b13fa46c
Ryan <74019611+Ryan-McKee2001@users.noreply.github.com>
2023-06-05 11:02:31 +0100

    updates

--- API/staffProject/src/main/java/com/example/staffProject/repository/MySQLData.java
+++
@@ -1,7 +0,0 @@
-package com.example.staffProject.repository;
-
-import com.example.staffProject.models.Management;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MySQLData extends JpaRepository<Management, Integer> {
-}
--- API/staffProject/src/test/java/com/example/staffProject/helper/statusCodes.java
+++
@@ -1,7 +0,0 @@
-package com.example.staffProject.helper;
-
-public class statusCodes {
-    public static final int OK_STATUS_CODE = 200;
-    public static final int INTERNAL_SERVER_ERROR_STATUS_CODE = 500;
-    public static final int CLIENT_ERROR_RESPONSE_CODE = 400;
-}
